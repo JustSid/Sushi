483e648d308fc3a69c1f21ef0fbf45ed795c33c0
Nils Daumann <nils_daumann@slindev.com>
2017-01-22 13:28:25 +0100

    New level tiles and level number as part of the UI.

--- src/ingameui.asm
+++ src/ingameui.asm
@@ -6,44 +6,70 @@ updateIngameUI:
 	cp 0
 	jr nz, .greater0
 
-	ld hl, _SCRN0 + 32*2 + 18
+	ld hl, _SCRN0 + 32*14 + 18
 	ld a, 29
 	ld [hl+], a
 	ld a, 30
 	ld [hl], a
-	ld hl, _SCRN0 + 32*3 + 18
+	ld hl, _SCRN0 + 32*15 + 18
 	ld a, 31
 	ld [hl+], a
 	ld a, 32
 	ld [hl], a
-	ret
+	jr .endFish
 
 .greater0:
 	cp 1
 	jr nz, .greater1
-	ld hl, _SCRN0 + 32*2 + 18
+	ld hl, _SCRN0 + 32*14 + 18
 	ld a, 33
 	ld [hl+], a
 	ld a, 30
 	ld [hl], a
-	ld hl, _SCRN0 + 32*3 + 18
+	ld hl, _SCRN0 + 32*15 + 18
 	ld a, 35
 	ld [hl+], a
 	ld a, 32
 	ld [hl], a
-	ret
+	jr .endFish
 
 .greater1:
-	ld hl, _SCRN0 + 32*2 + 18
+	ld hl, _SCRN0 + 32*14 + 18
 	ld a, 33
 	ld [hl+], a
 	ld a, 34
 	ld [hl], a
-	ld hl, _SCRN0 + 32*3 + 18
+	ld hl, _SCRN0 + 32*15 + 18
 	ld a, 35
 	ld [hl+], a
 	ld a, 36
 	ld [hl], a
+
+.endFish:
+	ld a, [CurrentLevelNumber]
+	cp 1
+	jr nz, .level02
+
+	ld hl, _SCRN0 + 32*17 + 18
+	ld a, 37
+	ld [hl+], a
+	ld a, 38
+	ld [hl], a
+	jr .endLevel
+
+.level02:
+	cp 2
+	jr nz, .level03
+	ld hl, _SCRN0 + 32*17 + 18
+	ld a, 37
+	ld [hl+], a
+	ld a, 39
+	ld [hl], a
+	jr .endLevel
+
+.level03
+
+.endLevel:
 	ret
 
 ENDC
